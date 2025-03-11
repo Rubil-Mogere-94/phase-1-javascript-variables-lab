bc34ea5393fca394c4cc5ca218311e40e9c68461
Rubil <mogererubil@gmail.com>
2025-03-11 10:30:01 +0300

    new commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file!
+// Code your solution in this file!
+const companyName = 'Scuber'; // Fix for failing test 1
+let mostProfitableNeighborhood = 'Chelsea'; // Fix for failing test 2 & 3
+let companyCeo = 'Susan Smith'; // Fix for failing test 4 & 5
